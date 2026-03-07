129c4d142974ff0cfc40cdc7f80e6d730895701c
Nasrudin <muhammadnasrudin674@gmail.com>
2026-03-08 00:59:02 +0700

    implement main navigasi dan menambahkan search

--- pilem/lib/screens/home_screen.dart
+++ pilem/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pilem/models/movie.dart';
+import 'package:pilem/screens/detail_screen.dart';
 import 'package:pilem/services/api_services.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -74,29 +75,38 @@ class _HomeScreenState extends State<HomeScreen> {
             itemCount: movies.length,
             itemBuilder: (BuildContext context, int index) {
               final Movie movie = movies[index];
-              return Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Image.network(
-                      'https://image.tmdb.org/t/p/w500${movie.posterPath}',
-                      height: 150,
-                      width: 100,
-                      fit: BoxFit.cover,
+              return GestureDetector(
+                onTap: () =>
+                  Navigator.push(
+                    context, 
+                    MaterialPageRoute(
+                      builder: (context) => DetailScreen(movie: movie),
                     ),
-                    SizedBox(height: 5),
-                    Text(
-                      movie.title,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
+                  ),
+              child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Image.network(
+                          "https://image.tmdb.org/t/p/w500${movie.posterPath}",
+                          width: 100,
+                          height: 150,
+                          fit: BoxFit.cover,
+                        ),
+                        Text(
+                          movie.title.length > 14
+                              ? '${movie.title.substring(0, 10)}...'
+                              : movie.title,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              );
-            },
-          ),
-        ),
+                  ),
+                );
+              }),
+        )
       ],
     );
   }
